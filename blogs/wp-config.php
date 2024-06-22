efa26a73eb4cdb13e5c7480568ee4732eceed6b5
Jevin Ta <jevint1267@gmail.com>
2024-06-22 17:36:24 -0400

    Configuring wordpress

--- blogs/wp-config.php
+++ blogs/wp-config.php
@@ -83,7 +83,8 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
-
+define( 'WP_HOME', 'http://localhost:8888/wordpress' );
+define( 'WP_SITEURL', 'http://localhost:8888/wordpress' );
 
 /* That's all, stop editing! Happy publishing. */
 
